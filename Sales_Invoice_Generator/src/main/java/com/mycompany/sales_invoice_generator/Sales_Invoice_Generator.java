98156292ebf66df3c1a9cd88b6889d23c4ff1bf8
20194841 <20194841@FUE.EDU.EG>
2022-10-10 14:33:09 +0200

    project_initialization

---
+++ Sales_Invoice_Generator/src/main/java/com/mycompany/sales_invoice_generator/Sales_Invoice_Generator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sales_invoice_generator;
+
+public class Sales_Invoice_Generator {
+
+    public static void main(String[] args) {
+        System.out.print("second run ");
+    }
+}
