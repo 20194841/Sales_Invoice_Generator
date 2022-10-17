551e8363a9576ac864a4e7b46ff3d2c1bffeb330
20194841 <20194841@FUE.EDU.EG>
2022-10-17 09:26:13 +0200

    Update GUI.java
    GUI FINISHED

--- Sales_Invoice_Generator/src/main/java/View/GUI.java
+++ Sales_Invoice_Generator/src/main/java/View/GUI.java
@@ -4,13 +4,13 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
-
 public class GUI {
     public void Main_Frame(){
-         String data[][]={ {"101","Amit","670000"},    
-                          {"102","Jai","780000"},    
-                          {"101","Sachin","700000"}};    
-          String column[]={"ID","NAME","SALARY"};    
+        //Table data diclaration and initialaization
+        String HeaderColumn[]={"NO.","DATE","NAME","TOTAL"};
+        String HeaderData[][]={ {"101","Amit","670000","670000"},{"102","Jai","780000","670000"},{"101","Sachin","700000","670000"}};                           
+        String LineColumn[]={"NO.","ITEM NAME","ITEM PRICE","COUNT","ITEM TOTAL"};  
+        String LineData[][]={ {"101","Amit","670000","Amit","670000"},{"102","Jai","780000","Amit","670000"},{"101","Sachin","700000","Amit","670000"}};                           
           
         //Creating Objects
         JFrame frame = new JFrame("Sales Invoice Generator");
@@ -22,8 +22,8 @@ public class GUI {
         JPanel RightSouth = new JPanel();
         JPanel LeftCenter = new JPanel();
         JPanel LeftSouth = new JPanel();
-
-
+        JTable HeaderTable = new JTable();
+        JTable LineTable = new JTable();
         JMenuBar menu = new JMenuBar();
         JToolBar toolbar = new JToolBar();
        
@@ -37,14 +37,16 @@ public class GUI {
         toolbar.add(menu);
     
         //labels declaration and initialaization
-        JLabel LeftLabel = new JLabel("Invoice Table");
-        JLabel RightLabel_1 = new JLabel("Invoice Number");
+        JLabel LeftLabel = new JLabel("  Invoice Table");
+        JLabel RightLabel_1 = new JLabel(" Invoice Number");
         JLabel RightLabel_1_1 = new JLabel("23");
-        JLabel RightLabel_2 = new JLabel("Invoice Date");
+        JLabel RightLabel_2 = new JLabel(" Invoice Date");
         JLabel RightLabel_2_1 = new JLabel("10/10/2020");
-        JLabel RightLabel_3 = new JLabel("Customer Name");
-        JLabel RightLabel_3_1 = new JLabel("Mohamed Elbadri");
-        JLabel RightLabel_4 = new JLabel("Invoice Items");
+        JLabel RightLabel_3 = new JLabel(" Customer Name");
+        JLabel RightLabel_3_1 = new JLabel(" Mohamed Elbadri");
+        JLabel RightLabel_4 = new JLabel(" Total");
+        JLabel RightLabel_4_1 = new JLabel(" 50"+"  LE");
+        JLabel RightLabel_5 = new JLabel(" Invoice Items");
         //Separators declaration 
         JLabel NewLine_1 = new JLabel(" ");
         JLabel NewLine_2 = new JLabel(" ");
@@ -55,13 +57,20 @@ public class GUI {
         JLabel NewLine_7 = new JLabel(" ");
         JLabel NewLine_8 = new JLabel(" ");
 
-
         //Button declaration and initialaization
         JButton creat = new JButton("Create New Invoice");
         JButton delete = new JButton("Delet Invoice");       
         JButton save = new JButton("Save");
         JButton cancel = new JButton("Cancel");
         
+        //Table initialaization
+        HeaderTable = new JTable(HeaderData, HeaderColumn);
+        LineTable = new JTable(LineData, LineColumn);
+        JScrollPane LeftTable = new JScrollPane(HeaderTable);
+        JScrollPane RightTable = new JScrollPane(LineTable);
+        HeaderTable.setFillsViewportHeight(true);
+        LineTable.setFillsViewportHeight(true);
+
         //Main panel declaration and initialization
         panel.setLayout(new GridLayout(1,2));
         panel.add(LeftPanel);
@@ -72,7 +81,7 @@ public class GUI {
         LeftPanel.setLayout(new BorderLayout());
         LeftSouth.setLayout(new FlowLayout());
         RightSouth.setLayout(new FlowLayout());
-        RightTop.setLayout(new GridLayout(8,2));
+        RightTop.setLayout(new GridLayout(9,2));
 
         //Set border to the panels
         LeftPanel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -91,15 +100,19 @@ public class GUI {
         RightTop.add(RightLabel_3_1);
         RightTop.add(NewLine_5);            //separator
         RightTop.add(NewLine_6);            //separator 
+        RightTop.add(RightLabel_4);
+        RightTop.add(RightLabel_4_1);
         RightTop.add(NewLine_7);            //separator
         RightTop.add(NewLine_8);            //separator 
-        RightTop.add(RightLabel_4);
+        RightTop.add(RightLabel_5);
         RightSouth.add(save);
         RightSouth.add(cancel);
-        
-        // Components Added using Flow Layout
+        RightCenter.add(RightTable);
+
+        // Components Added in the left panel
         LeftSouth.add(creat);
-        LeftSouth.add(delete);
+        LeftSouth.add(delete);     
+        LeftCenter.add(LeftTable);
 
         // Add componants to the panel
         RightPanel.add(BorderLayout.NORTH, RightTop);
@@ -114,8 +127,6 @@ public class GUI {
         frame.setSize(1000, 600);
         frame.getContentPane().add(BorderLayout.NORTH, toolbar);
         frame.getContentPane().add(panel);
-        frame.setVisible(true);
-        
-    }
-    
+        frame.setVisible(true); 
+    }  
 }
